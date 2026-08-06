64292281ab53bb35eadfa0dbdbb0f45c8741791c
fazal rehman <fr7479960@gmail.com>
2026-08-06 21:19:41 +0500

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import Dashboard from './pages/Dashboard';
 import AddApplication from './pages/AddApplication';
 import AllApplications from './pages/AllApplications';
 import ApiJobs from './pages/ApiJobs';
+import { Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    
+    path: "/", element: <App />,
+    
     children: [
+      {index: true, element: <Navigate to="/add-application" replace /> },
       { path: "dashboard", element: <Dashboard /> },
       { path: "add-application", element: <AddApplication /> },
       { path: "all-applications", element: <AllApplications /> },
